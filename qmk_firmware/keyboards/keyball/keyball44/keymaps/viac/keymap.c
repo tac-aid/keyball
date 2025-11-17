cd9566ab9e18ed43b7c8545af9dd7d0691ba651a
tac-aid <84469143+tac-aid@users.noreply.github.com>
2025-11-17 15:30:32 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/viac/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/viac/keymap.c
@@ -72,6 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #    include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
+  keyball_oled_render_keyinfo();
+  keyball_oled_render_ballinfo();
   keyball_oled_render_layerinfo();
 }
 #endif
